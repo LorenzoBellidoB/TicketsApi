e2569d7b41e578af26da868a0f00e510b65abd1f
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2025-06-26 00:09:56 +0200

    clsDependiente

--- ENT/clsDependiente.cs
+++ ENT/clsDependiente.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ENT
 {
+    [Table("dependientes")]
     public class clsDependiente
     {
         #region Propiedades
+        [Key]
+        [Column("id")]
         public int IdDependiente { get; set; }
-
+        [Column("nombre")]
         public string Nombre { get; set; }
-
+        [Column("empresa_id")]
         public int IdEmpresa { get; set; }
 
         #endregion
