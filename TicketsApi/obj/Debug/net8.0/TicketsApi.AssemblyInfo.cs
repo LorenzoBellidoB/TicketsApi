3d890f4a7c76b2a91b8c4c7d186c1e83e288bb56
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2025-07-09 12:44:16 +0200

    ahora si

--- TicketsApi/obj/Debug/net8.0/TicketsApi.AssemblyInfo.cs
+++ TicketsApi/obj/Debug/net8.0/TicketsApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5b424e4a1a4f2bb156c9306d4f0a398e1a13373")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fd8937d7db726c8faeed2bb7e9aa0051a601cce")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
