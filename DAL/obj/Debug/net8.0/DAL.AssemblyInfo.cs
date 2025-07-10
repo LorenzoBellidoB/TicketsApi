42b1c8dff2cb86505f9f4914f312059cc9b2cc95
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2025-07-10 21:00:35 +0200

    dir

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b367df809dec2139e9c6f2a1443871f2d7bcda9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a095c899f97beade145abc6e8eaea835d69c2230")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
