c95db8c1dbc6afc9d2fb3278a8b82cd72baca800
LorenzoBellido <145675169+LorenzoBellido@users.noreply.github.com>
2025-07-15 12:18:07 +0200

    ent

--- DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+971669ffc4f98090e4c16f0d873f20f97800e5a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc03b054083ca7debd0d9bbe01ebe9df3b6199f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ENT/clsEmpresa.cs
+++ ENT/clsEmpresa.cs
@@ -22,6 +22,10 @@ namespace ENT
         public string Localidad { get; set; }
         [Column("provincia")]
         public string Provincia { get; set; }
+        [Column("telefono")]
+        public string Telefono { get; set; }
+        [Column("correo")]
+        public string Correo { get; set; }
         [Column("direccion")]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public string Direccion { get; set; }
@@ -40,7 +44,7 @@ namespace ENT
             IdEmpresa = idEmpresa;
         }
 
-        public clsEmpresa( string nombre, string cif, string calle, string codigo_postal, string localidad, string provincia, string direccion)
+        public clsEmpresa( string nombre, string cif, string calle, string codigo_postal, string localidad, string provincia, string direccion, string telefono, string correo)
         {
             Nombre = nombre;
             Cif = cif;
@@ -49,9 +53,11 @@ namespace ENT
             Localidad = localidad;
             Provincia = provincia;
             Direccion = direccion;
+            Telefono = telefono;
+            Correo = correo;
         }
 
-        public clsEmpresa(int idEmpresa, string nombre, string cif, string calle, string codigo_postal, string localidad, string provincia)
+        public clsEmpresa(int idEmpresa, string nombre, string cif, string calle, string codigo_postal, string localidad, string provincia, string telefono, string correo)
         {
             IdEmpresa = idEmpresa;
             Nombre = nombre;
@@ -60,9 +66,11 @@ namespace ENT
             Codigo_postal = codigo_postal;
             Localidad = localidad;
             Provincia = provincia;
+            Telefono = telefono;
+            Correo = correo;
         }
 
-        public clsEmpresa(int idEmpresa, string nombre, string cif, string calle, string codigo_postal, string localidad, string provincia, string direccion)
+        public clsEmpresa(int idEmpresa, string nombre, string cif, string calle, string codigo_postal, string localidad, string provincia, string direccion, string telefono, string correo)
         {
             IdEmpresa = idEmpresa;
             Nombre = nombre;
@@ -72,6 +80,8 @@ namespace ENT
             Localidad = localidad;
             Provincia = provincia;
             Direccion = direccion;
+            Telefono = telefono;
+            Correo = correo;
         }
 
         #endregion
